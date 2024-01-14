dff4539b1d61d965294fe0261e430832ab19a35a
anchenrc <156558792+anchenrc@users.noreply.github.com>
2024-01-14 15:57:01 +0800

    Update train.py

--- pytorch_classification/vision_transformer/train.py
+++ pytorch_classification/vision_transformer/train.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
     parser.add_argument('--model-name', default='', help='create model name')
 
     # 预训练权重路径，如果不想载入就设置为空字符
-    parser.add_argument('--weights', type=str, default='./vit_base_patch16_224_in21k.pth',
+    parser.add_argument('E:\github_data\DeepLearning-pth', type=str, default='./vit_base_patch16_224_in21k.pth',
                         help='initial weights path')
     # 是否冻结权重
     parser.add_argument('--freeze-layers', type=bool, default=True)
